7d48525b98520e3acbc9b1c8290a7551b72a91cc
Justin Clark-Casey <justincc@justincc.org>
2013-08-22 23:41:28 +0100

    Add command line parameters to ruplg.py

--- reset-user-password-line-generator/src/ruplg.py
+++ reset-user-password-line-generator/src/ruplg.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
-password = "changeme"
+from sys import argv
 
-for i in range(220):
-  print "reset user password ima bot_%s %s" % (i, password)
+if len(argv) != 5:
+  print "Usage: %s <first-name> <last-name-stub> <password> <no-of-bots>" % argv[0]
+  exit(-1)
+
+firstName = argv[1]
+lastNameStub = argv[2]
+password = argv[3]
+botCount = int(argv[4])
+
+for i in range(botCount):
+  print "reset user %s %s_%s %s" % (firstName, lastNameStub, i, password)
