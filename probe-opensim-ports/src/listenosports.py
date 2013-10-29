89dcdaee6abf0df6df46cf7160bb6762d4b03e54
Justin Clark-Casey <justincc@justincc.org>
2013-10-29 21:30:12 +0000

    Add beginnings of a pair of programs, one to listen to ports and ip addresses specified in opensim config files and another to probe

---
+++ probe-opensim-ports/src/listenosports.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+HOST_NAME = "192.168.1.2"
+PORT_NUMBER = 9000
+
+import BaseHTTPServer
+import time
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/plain")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/plain")
+        s.end_headers()
+        s.wfile.write("OK")
+
+if __name__ == '__main__':
+    server = BaseHTTPServer.HTTPServer
+    httpd = BaseHTTPServer.HTTPServer((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
