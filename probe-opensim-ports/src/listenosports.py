014791dab97cd7816f6d5b7db38f47b9be689e45
Justin Clark-Casey <justincc@justincc.org>
2013-10-30 00:56:11 +0000

    Make listenosports.py parse an example OpenSim.ini file and use http_listener_port if available
    Need to factor code into common class and get probe to also use

--- probe-opensim-ports/src/listenosports.py
+++ probe-opensim-ports/src/listenosports.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 
+from io import StringIO
 import BaseHTTPServer
+import ConfigParser
 import time
 
 HOST_NAME = "0.0.0.0"
-PORT_NUMBER = 9000
+DEFAULT_PORT = 9000
+IniNetworkSection = "Network"
+IniHttpListenerPort = "http_listener_port"
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_HEAD(s):
@@ -19,12 +23,42 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     s.wfile.write("OK")
 
 if __name__ == '__main__':
+  # Need to strip leading whitespace from config file - ConfigParser hates this!
+  output = StringIO()
+
+  with open("config/OpenSim.ini", "r") as fh:
+    lines = fh.readlines()
+
+  for line in lines:
+    line = line.lstrip()
+    output.write(unicode(line))
+
+  output.seek(0)
+
+  opensimIniConfig = ConfigParser.RawConfigParser()
+  #opensimIniConfig.read('config/OpenSim.ini')
+
+  opensimIniConfig.readfp(output)
+
+  # print opensimIniConfig.sections()
+
+  if opensimIniConfig.has_section(IniNetworkSection):
+    if opensimIniConfig.has_option(IniNetworkSection, IniHttpListenerPort):
+      portNumber = opensimIniConfig.getint(IniNetworkSection, IniHttpListenerPort)
+      print "Probing HTTP port number %s" % portNumber
+    else:
+      portNumber = DEFAULT_PORT
+      print "No %s found in [%s], using default port %s" % (IniHttpListenerPort, IniNetworkSection, portNumber)
+  else:
+    portNumber = DEFAULT_PORT
+    print "No [%s] section found, using default port %s" % (IniNetworkSection, portNumber)
+  
   server = BaseHTTPServer.HTTPServer
-  httpd = BaseHTTPServer.HTTPServer((HOST_NAME, PORT_NUMBER), MyHandler)
-  print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+  httpd = BaseHTTPServer.HTTPServer((HOST_NAME, portNumber), MyHandler)
+  print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, portNumber)
   try:
       httpd.serve_forever()
   except KeyboardInterrupt:
       pass
   httpd.server_close()
-  print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+  print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, portNumber)
