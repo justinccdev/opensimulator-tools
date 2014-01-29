cc4094decb8efae55e1dda58245b481181469c8c
Justin Clark-Casey <justincc@justincc.org>
2014-01-29 21:00:05 +0000

    Add simple opensim start script as alternative to init.d version.  Robust side to follow.

---
+++ infrastructure/control/simple/README.md
@@ -0,0 +1,8 @@
+# README #
+
+A very simple script for starting components of OpenSimulator as a user rather
+than root.  Incorporates checks to avoid starting a component instance if one
+is already running
+
+Starts named screen instances for the components.  To shutdown the simulator
+you need to connect to the appropriate screen instance and do this manually.
---
+++ infrastructure/control/simple/src/start-opensim.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+### CONFIGURE THESE PATHS ###
+my $binaryPath = "/home/opensim/opensim/opensim-current/bin";
+my $pidPath = "/tmp/OpenSim.pid";
+### END OF CONFIG ###
+
+-f $pidPath and die "ERROR: OpenSim PID file $pidPath still present.  Assuming OpenSim has been started already.\n";
+
+# If PID isn't set then we'll check the screen list.  
+# However, this is a much less perfect mechanism since OpenSimulator may have been started outside screen
+`screen -list` =~ /OpenSim/ and die "ERROR: Screen session for OpenSim already started.\n";
+
+chdir($binaryPath) or die;
+execCmd("screen -S OpenSim -d -m mono --debug OpenSim.exe");
+
+# I would like to perform this check but it generates false positives and I would rather not put in a messy sleep
+# unless (-f $pidPath) { warn "WARNING: OpenSimulator PID file $pidPath not found after startup.  Set this in [Startup] PIDFile in OpenSim.ini"; }
+
+if (`screen -list` =~ /OpenSim/)
+{
+    print "OpenSim started.\n";
+}
+else
+{
+    die "ERROR: OpenSim did not start.\n";
+}
+
+sub execCmd
+{
+    my $cmd = $_[0];
+    print "Excecuting command $cmd\n";
+    system ($cmd) && die;
+}
