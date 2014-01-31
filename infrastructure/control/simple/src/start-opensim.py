1a48f85e04f9192ccf9ba675ff8ddd5719752950
Justin Clark-Casey <justincc@justincc.org>
2014-01-31 00:15:03 +0000

    Add screen -list check, fix initial pid check

--- infrastructure/control/simple/src/start-opensim.py
+++ infrastructure/control/simple/src/start-opensim.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import os.path
+import re
+import subprocess
 import sys
 
 ### CONFIGURE THESE PATHS ###
@@ -8,6 +10,19 @@ binaryPath = "/home/opensim/opensim/opensim-current/bin"
 pidPath = "/tmp/OpenSim.pid"
 ### END OF CONFIG ###
 
-if not os.path.exists(pidPath):
+if os.path.exists(pidPath):
   print >> sys.stderr, "ERROR: OpenSim PID file %s still present.  Assuming OpenSim has been started already." % pidPath
+  sys.exit(1)
+
+# If PID isn't set then we'll check the screen list.  
+# However, this is a much less perfect mechanism since OpenSimulator may have been started outside screen
+screenList = ""
+
+try:
+  screenList = subprocess.check_output("screen -list", shell=True)
+except:
+  None
+
+if re.match("\s+\d+\.OpenSim", screenList):
+  print >> sys.stderr, "ERROR: Screen session for OpenSim already started."
   sys.exit(1)
