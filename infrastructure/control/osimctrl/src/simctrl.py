67d368c284dd7b641a204b37c26b729d4db64e39
Justin Clark-Casey <justincc@justincc.org>
2014-02-03 21:04:37 +0000

    refactor start code into startComponent function

--- infrastructure/control/osimctrl/src/simctrl.py
+++ infrastructure/control/osimctrl/src/simctrl.py
@@ -11,7 +11,7 @@ pidPath = "/tmp/OpenSim.pid"
 screenName = "OpenSim"
 ### END OF CONFIG ###
 
-### FUNCTIONS ###
+### UTILITY FUNCTIONS ###
 def chdir(dir):
   os.chdir(dir)
   print "Executing chdir to %s" % dir
@@ -35,29 +35,33 @@ def findScreen(screenName):
   
 def getScreenList():
   return execCmd("screen -list")
-        
+
+### MAIN FUNCTIONS ###
+def startComponent():
+  if os.path.exists(pidPath):
+    print >> sys.stderr, "ERROR: OpenSim PID file %s still present.  Assuming OpenSim has been started already." % pidPath
+    sys.exit(1)
+  
+  # If PID isn't set then we'll check the screen list.  
+  # However, this is a much less perfect mechanism since OpenSimulator may have been started outside screen
+  if findScreen(screenName):
+    print >> sys.stderr, "ERROR: Screen session for OpenSim already started."
+    sys.exit(1)
+    
+  chdir(binaryPath)
+  
+  execCmd("screen -S OpenSim -d -m mono --debug OpenSim.exe")
+  
+  screen = findScreen(screenName)
+  if screen != None:
+    print "OpenSim starting in screen instance %s" % screen.group(1)
+  else:
+    print >> sys.stderr, "ERROR: OpenSim did not start."
+    exit(1)  
+  
 ### SCRIPT ###
 if len(sys.argv) < 2 or sys.argv[1] != "start":
   print >> sys.stderr, "Usage: %s start" % sys.argv[0]
   sys.exit(2)
   
-if os.path.exists(pidPath):
-  print >> sys.stderr, "ERROR: OpenSim PID file %s still present.  Assuming OpenSim has been started already." % pidPath
-  sys.exit(1)
-
-# If PID isn't set then we'll check the screen list.  
-# However, this is a much less perfect mechanism since OpenSimulator may have been started outside screen
-if findScreen(screenName):
-  print >> sys.stderr, "ERROR: Screen session for OpenSim already started."
-  sys.exit(1)
-  
-chdir(binaryPath)
-
-execCmd("screen -S OpenSim -d -m mono --debug OpenSim.exe")
-
-screen = findScreen(screenName)
-if screen != None:
-  print "OpenSim starting in screen instance %s" % screen.group(1)
-else:
-  print >> sys.stderr, "ERROR: OpenSim did not start."
-  exit(1)
+startComponent()
