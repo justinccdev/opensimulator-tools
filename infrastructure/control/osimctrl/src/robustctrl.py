8559430aeaaf9c693bf9acac89ed608eb0777089
Justin Clark-Casey <justincc@justincc.org>
2014-02-04 23:50:53 +0000

    Add --help by using argparse to parse command line arguments instead of hand-rolling

--- infrastructure/control/osimctrl/src/robustctrl.py
+++ infrastructure/control/osimctrl/src/robustctrl.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
 
+import argparse
 import sys
 import osimctrl.ctrl as osc
 
 ### CONFIGURE THESE PATHS ###
+
+# The path to your OpenSimulator binary directory
 binaryPath = "/home/opensim/opensim/opensim-current/bin"
+
+# It isn't critical to have a PID file, but this will allow the script to detect if the component is running outside
+# of a screen instance
 pidPath = "/tmp/Robust.pid"
+
 ### END OF CONFIG ###
 
 componentName = "Robust"
 screenName = componentName
 
 ### SCRIPT ###
-if len(sys.argv) < 2 or not sys.argv[1] in osc.commands:
-  print >> sys.stderr, "Usage: %s %s" % (sys.argv[0], "|".join(osc.commands))
-  sys.exit(2)
+parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter)
 
-osc.execCommand(sys.argv[1], binaryPath, pidPath, componentName, screenName)
+# TODO: construct help automatically from ctrl module
+parser.add_argument('command', choices = osc.commands, metavar = '<command>', help = "attach - Attach to screen process for this component if running.\nstart  - Start this component in a screen process.\nstatus - Status of this component")
+
+opts = parser.parse_args()
+
+osc.execCommand(opts.command, binaryPath, pidPath, componentName, screenName)
