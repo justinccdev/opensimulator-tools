ad878e63713ee238054151cba46879857ea15ffe
Justin Clark-Casey <justincc@justincc.org>
2014-02-05 00:20:07 +0000

    Use subpprocess.Popen() to execute command since subprocess.check_output() was only introduced in python 2.7

--- infrastructure/control/osimctrl/src/osimctrl/ctrl.py
+++ infrastructure/control/osimctrl/src/osimctrl/ctrl.py
@@ -16,7 +16,11 @@ def checkPid(pidPath):
   
 def execCmd(cmd):  
   print "Executing command: %s" % cmd
-  output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+
+  # Use Popen instead of subprocess.check_output as latter only exists in Python 2.7 onwards
+  # output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)  
+  output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+  
   # print "For output got: %s" % output
   return output
 
