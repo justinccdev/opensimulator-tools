e60b906b605c74dd345042f5a2a323269e2f45ad
Justin Clark-Casey <justincc@justincc.org>
2014-02-19 16:34:39 +0000

    Any uncaught signal (including SIGUSR1) will terminate mono.  So only send if child command name is bash

--- infrastructure/control/osimctrl/src/osimctrl/osimctrl.py
+++ infrastructure/control/osimctrl/src/osimctrl/osimctrl.py
@@ -144,12 +144,11 @@ class OSimCtrl:
       print >> sys.stderr, "No screen session named %s to stop" % self._screenName
       return False
 
-    # If we're using the autorestart script then we need to sent it a SIGTERM first to stop it restarting
-    # the component
-    # FIXME: For now, always send a SIGUSR1.  If we are not using the autorestart script then this will be received
-    # by the OpenSimulator mono instance instead, where it will be ignored.
-    autoRestartPid = int(execCmd("ps --ppid %s -o pid=" % screen.split(".")[0]))
-    os.kill(autoRestartPid, signal.SIGUSR1)
+    (autoRestartPid, comm) = execCmd("ps --ppid %s -o pid=,comm=" % screen.split(".")[0]).split()
+    
+    # Any uncaught signal sent to mono (including SIGUSR1) will kill it
+    if comm == "bash":
+      os.kill(int(autoRestartPid), signal.SIGUSR1)
           
     execCmd("%s -S %s -p 0 -X stuff quit$(printf \r)" % (self._screenPath, self._screenName))
     
