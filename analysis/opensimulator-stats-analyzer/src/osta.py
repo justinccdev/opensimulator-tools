fd1a3c297777225ea64040789e7891424d35fabb
Justin Clark-Casey <justincc@justincc.org>
2014-07-16 02:13:17 +0100

    Always print out absolute values but also print out deltas where available
    This is because with some change over time stats, absolute value is useful (i.e. packetInbox) whereas on others it isn't, very (HTTPRequestsMade)
    Need to clean up this shoddy code, only committing because at least it works and it's late at night,
    Also need to add in unit names to output where applicable

--- analysis/opensimulator-stats-analyzer/src/osta.py
+++ analysis/opensimulator-stats-analyzer/src/osta.py
@@ -9,32 +9,76 @@ if len(sys.argv) <= 1:
     sys.exit(1)
 
 #lineRe = re.compile("(.* .*) - (.*) : (\d+)[ ,]([^:]*)")
-lineRe = re.compile("(.* .*) - (.*) : ([\d\.-]+)(?:\D+)?([\d\.-]+)?")
-data = {}
+#lineRe = re.compile("(.* .*) - (.*) : (?P<abs>[\d\.-]+)(?: (?:\D+))?(?P<delta>[\d\.-]+)?")
+lineRe = re.compile("(.* .*) - (.*) : (?P<abs>[^,]+)(?:, )?(?P<delta>[^,]+)?")
+valueRe = re.compile("([^ %/]+)")
+
+dataAbs = {}
+dataDelta = {}
 
 with open(sys.argv[1]) as f:
     for line in f:    
         match = lineRe.match(line)
         
         if match != None:
-            statFullName = match.group(2)
-            
+            statFullName = match.group(2) 
+
+            """            
             # If this is a single value or a percentage, then only first number group will match and
             # that's the value we want.
             # If this is a change over time stat, then the second number group will match and that's the one we want
-            value = match.group(match.lastindex)
+            if not match.group("delta"):
+                value = match.group("abs")
+            else:
+                value = match.group("delta")
+            """          
             
-                # print "%s: %s" % (statFullName, value)
+            rawValue = match.group("abs")
+            #print match.lastindex
+            #print rawValue
             
-            if not statFullName in data:
-                data[statFullName] = []                
+            """
+            if not match.group("delta"):
+                rawValue = match.group("abs")
+            else:
+                rawValue = match.group("delta")
+            """
                 
-            data[statFullName].append(float(value))
+            valueMatch = valueRe.match(rawValue)
+            value = valueMatch.group(1)            
+            
+            
+                # print "%s: %s" % (statFullName, value)                
+            
+            if not statFullName in dataAbs:
+                dataAbs[statFullName] = []                
+                
+            dataAbs[statFullName].append(float(value))
+            
+            # Handle delta value if present
+            if match.lastindex > 3 and match.group("delta"):
+                rawValue = match.group("delta")
+                
+                valueMatch = valueRe.match(rawValue)
+                value = valueMatch.group(1)
+                
+                if not statFullName in dataDelta:
+                    dataDelta[statFullName] = []                
+                    
+                dataDelta[statFullName].append(float(value))                
+                
         #else:
         #    print "Ignoring [%s]" % (line)
             
-longestKey = max(data, key = len)
+longestKey = max(dataAbs, key = len)
     
-for statName, values in sorted(data.items()):
+for statName, values in sorted(dataAbs.items()):
     # print "%s: %s" % (stat, ", ".join(values))
-    print "%-*s: %s to %s" % (len(longestKey), statName, min(values), max(values))
+    sys.stdout.write("%-*s: %s to %s" % (len(longestKey), statName, min(values), max(values)))
+    
+    if statName in dataDelta:
+        values = dataDelta[statName]
+        print ", %s to %s" % (min(values), max(values))
+    else:
+        print
+         
