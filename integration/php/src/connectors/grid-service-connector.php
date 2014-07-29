a442fc801a591dd2c1c9f64543e3f4006782026c
Justin Clark-Casey <justincc@justincc.org>
2014-07-30 00:35:30 +0100

    Add example program for get region flags call and necessary connector code

--- integration/php/src/connectors/grid-service-connector.php
+++ integration/php/src/connectors/grid-service-connector.php
@@ -3,6 +3,17 @@
 require_once "$IP/utils.php";
 require_once "$IP/connectors/connector-utils.php";
 
+function GetRegionFlags($serviceUri, $regionId, $debug = FALSE)
+{
+    $params 
+        = array(
+            'SCOPEID' => UUID_ZERO,
+            'REGIONID' => $regionId,
+            'METHOD' => "get_region_flags");   
+            
+    return PostToService($serviceUri, http_build_query($params), $debug);            
+}
+
 function GetRegionRange($serviceUri, $minX, $minY, $maxX, $maxY, $debug = FALSE)
 {
     $params 
