ca65d9dbd5ee83e3dcf7fc2a8a227e23d381bcd9
Justin Clark-Casey <justincc@justincc.org>
2014-07-31 02:30:11 +0100

    minor: Complete comment about why we need to specify a user with permissions.

--- integration/php/src/programs/groups/update-group.php
+++ integration/php/src/programs/groups/update-group.php
@@ -31,6 +31,9 @@ $charter = $params->args['charter'];
 // and feed back the fields we don't want to change!
 $existingGroupE = GetGroupByName($GROUPS_SERVICE_URI, $groupId)->RESULT;
 
+// The update change can only be performed by a user with sufficient permissions, which the founder should have.
+// Can't get around this at the moment, even though this should be a voluntary perms check for simulator purposes, 
+// not mandatory.
 $existingFounderId = (string)$existingGroupE->FounderID;
 $existingGroupId = (string)$existingGroupE->GroupID;
 $existingGroupPictureId = (string)$existingGroupE->InsigniaID;
@@ -40,9 +43,9 @@ $existingOpenEnrollment = ToBool((string)$existingGroupE->OpenEnrollment);
 $existingMembershipFee = (int)$existingGroupE->MembershipFee;
 $existingShownInList = ToBool((string)$existingGroupE->ShownInList);
 
-echo "existingGroupId '$existingGroupId'\n";
-echo "existingGroupPictureId '$existingGroupPictureId'\n";
-echo "existingAllowPublish '$existingAllowPublish'\n";
+//echo "existingGroupId '$existingGroupId'\n";
+//echo "existingGroupPictureId '$existingGroupPictureId'\n";
+//echo "existingAllowPublish '$existingAllowPublish'\n";
 
 UpdateGroup(
     $GROUPS_SERVICE_URI, $existingGroupId, $existingFounderId, $charter, 
