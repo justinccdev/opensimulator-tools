9235472879e5f3d9383c0f2d0f23beea89e23bb6
Justin Clark-Casey <justincc@justincc.org>
2014-07-31 01:14:13 +0100

    Add first draft of add group example program

--- integration/php/src/connectors/groups-service-connector.php
+++ integration/php/src/connectors/groups-service-connector.php
@@ -63,4 +63,86 @@ function GetUserMemberships($serviceUri, $userUuid, $groupUuid, $debug = FALSE)
     return PostToService($serviceUri, http_build_query($params), $debug);
 }
 
+/*
+ * Add or update an existing group.
+ * 
+ * @param string $serviceURI 
+ *      Inventory service URI
+ * @param UUID $groupID 
+ *      UUID of this group.  
+ *      If it doesn't already exist then a new group is created.  
+ *      If it already exists then the group with that UUID is updated.
+ * @param string $groupName
+ *      Name of the group.  Ignored if the group already exists.
+ * @param UUID $founderID 
+ *      UUID of the user account of the founder of the group.  Ignored if the group already exists.*  
+ * @param string $charter
+ *      Charter information (description) for the group.
+ * @param UUID $insigniaID
+ *      Texture asset to use as the picture for the group.  If UUID.Zero then no picture will be available.
+ * @param boolean $allowPublish
+ *      In Linden Lab probably governs whether this group is published to search.
+ *      Very probably currently ignored in core OpenSimulator.
+ * @param boolean $maturePublish
+ *      In Linden Lab probably stops this group being shown to users in search unless they have Mature set in their preferences.
+ *      Very probably currently ignored in core OpenSimulator.
+ * @param int $membershipFee
+ *      The amount of virtual currency users will have to pay to join this group.  In OpenSimulator, unless you have
+ *      a currency module active you will normally want to set this to zero.
+ * @param bool $shownInList
+ *      Governs whether the group is shown to the user when they do a general search for groups.  Unfortunately, this
+ *      also governs whether it shows up in the FindGroups call.  Currently, set to true whenever possible, otherwise
+ *      the group information can only be retrieved via GetGroup.
+ *   
+ * @return SimpleXmlElement folder XML element 
+ */
+function AddGroup(
+    $serviceUri, $groupID, $groupName, $founderID, $charter, $groupPictureID, 
+    $allowPublish, $maturePublish, $openEnrollment, $membershipFee, $shownInList = TRUE,
+    $debug = FALSE)
+{
+    if (!is_bool($allowPublish))
+        throw new InvalidArgumentException("allowPublish '$allowPublish' is not a bool");
+    
+    if (!IsUuid($founderID))
+        throw new InvalidArgumentException("founderID '$founderID' is not a valid UUID");
+        
+    if (!IsUuid($groupID))
+        throw new InvalidArgumentException("groupID '$groupID' is not a valid UUID");
+    
+    if (!IsUuid($groupPictureID))
+        throw new InvalidArgumentException("groupPictureID '$groupPictureID' is not a valid UUID");
+    
+    if (!is_bool($openEnrollment))
+        throw new InvalidArgumentException("openEnrollment '$openEnrollment' is not a bool");
+    
+    if (!is_int($membershipFee) || $membershipFee < 0)
+        throw new InvalidArgumentException("membershipFee '$membershipFee' is not a positive or zero integer");
+        
+    if (!is_bool($shownInList))
+        throw new InvalidArgumentException("shownInList '$shownInList' is not a bool");
+    
+    $params
+        = array(
+            'RequestingAgentID' => UUID_ZERO,
+            'GroupID' => $groupID,
+            'GroupName' => $groupName,
+            'AllowPublish' => $allowPublish ? "true" : "false",
+            'MaturePublish' => $maturePublish ? "true" : "false",
+            'OpenEnrollment' => $openEnrollment ? "true" : "false",
+            'MembershipFee' => $membershipFee,
+            'Charter' => $charter,
+            'FounderID' => $founderID,
+            'InsigniaID' => $groupPictureID,
+            'ShownInList' => $shownInList ? "true" : "false",
+            // We need this to get around a bug in OpenSimulator 0.8 and before where not specifying some ServiceLocation 
+            // would set GroupName to "".  This is only used in Hypergrid so we can safely set this to a space.
+            'ServiceLocation' => " ", 
+            'METHOD' => 'PUTGROUP',
+            'OP' => 'ADD');
+            
+    $responseXml = PostToService($serviceUri, http_build_query($params), $debug);    
+}
+
+
 ?>
---
+++ integration/php/src/programs/groups/add-group.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'Console/CommandLine.php';
+require_once "../../config.php";
+require_once "../../utils.php";
+require_once "$IP/connectors/groups-service-connector.php";
+
+############
+### MAIN ###
+############
+
+$parser = new Console_CommandLine();
+
+$parser->addArgument('groupID');
+$parser->addArgument('groupName');
+$parser->addArgument('founderID');
+$parser->addArgument('charter');
+$parser->addArgument('groupPictureID');
+$parser->addArgument('openEnrollment');
+
+try
+{
+    $params = $parser->parse();
+}
+catch (Exception $e)
+{
+    $parser->displayError($e->getMessage());
+    exit(1);
+}
+
+$groupId = $params->args['groupID'];
+$groupName = $params->args['groupName'];
+$founderId = $params->args['founderID'];
+$charter = $params->args['charter'];
+$groupPictureID = $params->args['groupPictureID'];
+$openEnrollment = $params->args['openEnrollment'];
+
+AddGroup($GROUPS_SERVICE_URI, $groupId, $groupName, $founderId, $charter, $groupPictureID, TRUE, TRUE, TRUE, 0, TRUE, TRUE);
