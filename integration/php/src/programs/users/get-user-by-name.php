20753e5056465e6aeb71909a3901adf377bf9c61
Justin Clark-Casey <justincc@justincc.org>
2014-07-31 01:15:25 +0100

    Add get user by name example program

---
+++ integration/php/src/programs/users/get-user-by-name.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'Console/CommandLine.php';
+require_once "../../config.php";
+require_once "../../utils.php";
+require_once "$IP/connectors/useraccounts-service-connector.php";
+
+############
+### MAIN ###
+############
+
+$parser = new Console_CommandLine();
+
+$parser->addArgument('firstName');
+$parser->addArgument('lastName');
+        
+try
+{
+    $params = $parser->parse();
+}
+catch (Exception $e)
+{
+    $parser->displayError($e->getMessage());
+    exit(1);
+}
+
+$firstName = $params->args['firstName'];
+$lastName = $params->args['lastName'];
+
+GetUserAccountByName($USER_ACCOUNTS_SERVICE_URI, $firstName, $lastName, TRUE);
